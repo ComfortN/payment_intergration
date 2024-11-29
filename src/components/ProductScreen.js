0c0acc25d4f9dd0756107286ae348cb1c92f7157
Comfort Ngwenya <>
2024-11-29 13:26:08 +0200

    updating the products and the display

--- src/components/ProductScreen.js
+++ src/components/ProductScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image, Alert, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, Image, FlatList, Alert, TouchableOpacity } from 'react-native';
 import { colors } from '../constants/colors';
 
 
@@ -8,101 +8,118 @@ const ProductScreen = ({ navigation }) => {
         {
             id: 1,
             name: 'Leaf Bridal Jewelry Set',
-            price: 9.99,
+            price: 1999.99,
             description: 'Complete your bridal look with our exquisite Leaf Bridal Jewelry Set. Featuring a stunning necklace, earrings, bracelet, and ring, this set is adorned with clear cubic zirconia stones, designed to complement any wedding dress beautifully.',
             image: require('../../assets/rosegold.jpeg')
+        },
+        {
+            id: 2,
+            name: 'Crystal Pearl Earrings',
+            price: 97.99,
+            description: 'Elegant crystal and pearl drop earrings, perfect for adding a touch of sophistication to any outfit. Delicate and timeless design that complements both casual and formal wear.',
+            image: require('../../assets/pearl-earings.jpeg')
+        },
+        {
+            id: 3,
+            name: 'Vintage Charm Bracelet',
+            price: 12.99,
+            description: 'A stunning vintage-inspired charm bracelet with intricate detailing. Features multiple delicate charms and a classic silver finish, perfect for adding a nostalgic touch to your jewelry collection.',
+            image: require('../../assets/charm-bracelet.jpeg')
+        },
+        {
+            id: 4,
+            name: 'Gold Minimalist Necklace',
+            price: 800.99,
+            description: 'Sleek and modern gold-toned necklace with a simple pendant. Versatile design that easily transitions from day to night, making it a must-have in any jewelry collection.',
+            image: require('../../assets/gold-necklace.jpeg')
         }
         ];
+
+
+        const renderProduct = ({ item }) => (
+            <View style={styles.productCard}>
+                <Image source={item.image} style={styles.productImage} />
+                <Text style={styles.productName}>{item.name}</Text>
+                <Text style={styles.productPrice}>R{item.price}</Text>
+                {/* <Text style={styles.productDescription}>{item.description}</Text> */}
+                <TouchableOpacity 
+                    style={styles.buyButton}
+                    onPress={() => navigation.navigate('Checkout', { product: item })}
+                >
+                    <Text style={styles.buyButtonText}>Buy Now</Text>
+                </TouchableOpacity>
+            </View>
+        );
+
     
         return (
         <View style={styles.container}>
-            {products.map(product => (
-            <View key={product.id} style={styles.productCard}>
-                <Image source={product.image} style={styles.productImage} />
-                <Text style={styles.productName}>{product.name}</Text>
-                <Text style={styles.productPrice}>R{product.price}</Text>
-                <Text style={styles.productDescription}>{product.description}</Text>
-                <TouchableOpacity 
-                style={styles.buyButton}
-                onPress={() => navigation.navigate('Checkout', { product })}
-                >
-                <Text style={styles.buyButtonText}>Buy Now</Text>
-                </TouchableOpacity>
-            </View>
-            ))}
+            <FlatList
+                data={products}
+                renderItem={renderProduct}
+                keyExtractor={item => item.id.toString()}
+                numColumns={2}
+                columnWrapperStyle={styles.row}
+                contentContainerStyle={styles.listContainer}
+            />
         </View>
         );
     };
     
     const styles = StyleSheet.create({
         container: {
-          flex: 1,
-          padding: 20,
-          justifyContent: 'center',
-          alignItems: 'center',
-          backgroundColor: colors.background
+            flex: 1,
+            backgroundColor: colors.background
+        },
+        listContainer: {
+            padding: 10
+        },
+        row: {
+            flex: 1,
+            justifyContent: 'space-around'
         },
         productCard: {
-          width: '100%',
-          padding: 20,
-          borderWidth: 1,
-          borderColor: colors.primary,
-          borderRadius: 10,
-          alignItems: 'center',
-          backgroundColor: '#818D8B'
+            width: '45%',
+            padding: 10,
+            marginVertical: 10,
+            borderWidth: 1,
+            borderColor: colors.primary,
+            borderRadius: 10,
+            alignItems: 'center',
+            backgroundColor: '#818D8B'
         },
         productImage: {
-          width: 200,
-          height: 200,
-          resizeMode: 'contain'
+            width: 150,
+            height: 150,
+            resizeMode: 'contain'
         },
         productName: {
-          fontSize: 18,
-          fontWeight: 'bold',
-          marginVertical: 10,
-          color: colors.textPrimary,
+            fontSize: 16,
+            fontWeight: 'bold',
+            marginVertical: 10,
+            color: colors.textPrimary,
+            textAlign: 'center'
         },
         productPrice: {
-          fontSize: 16,
-          color: colors.textSecondary
+            fontSize: 14,
+            color: colors.textSecondary
         },
         productDescription: {
-          textAlign: 'center',
-          marginVertical: 10,
-          color: colors.primary
+            textAlign: 'center',
+            marginVertical: 10,
+            color: colors.primary,
+            fontSize: 12
         },
         buyButton: {
-          backgroundColor: colors.primary,
-          padding: 10,
-          paddingHorizontal: 30,
-          borderRadius: 5
+            backgroundColor: colors.primary,
+            padding: 10,
+            paddingHorizontal: 20,
+            borderRadius: 5
         },
         buyButtonText: {
-          color: 'white',
-          textAlign: 'center'
-        },
-        checkoutDetails: {
-          width: '100%',
-          alignItems: 'center'
-        },
-        checkoutTitle: {
-          fontSize: 24,
-          marginBottom: 20
-        },
-        payButton: {
-          backgroundColor: 'green',
-          padding: 15,
-          borderRadius: 5,
-          marginTop: 20
-        },
-        payButtonText: {
-          color: 'white',
-          textAlign: 'center'
-        },
-        successText: {
-          fontSize: 24,
-          color: 'green'
+            color: 'white',
+            textAlign: 'center',
+            fontSize: 12
         }
-      });
-      
+    });     
     export default ProductScreen;
