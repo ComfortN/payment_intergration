3063568ff193b7d55cedde6f476d324f7be954b8
Comfort Ngwenya <>
2024-11-29 10:55:48 +0200

    Adding the product screen route stack

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack'
 import SplashScreen from './src/components/SplashScreen';
+import ProductScreen from './src/components/ProductScreen';
 
 
 const Stack = createStackNavigator();
@@ -15,6 +16,11 @@ export default function App() {
           name="Splash" 
           component={SplashScreen} 
           options={{ headerShown: false }} 
+        />
+        <Stack.Screen 
+          name="Product" 
+          component={ProductScreen} 
+          options={{ title: 'Products' }} 
         />
         
       </Stack.Navigator>
