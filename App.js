9649a7fb215d1fad9fdd5bc188c1aecacdc1d5bb
Comfort Ngwenya <>
2024-11-29 10:49:25 +0200

    Adding the splash screen route stack

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack'
+import SplashScreen from './src/components/SplashScreen';
+
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Splash">
+        <Stack.Screen 
+          name="Splash" 
+          component={SplashScreen} 
+          options={{ headerShown: false }} 
+        />
+        
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
